8f8677d17b4fc51d8f3339ab528bf19925d5a798
Eti Sawhney <67235551+eti43@users.noreply.github.com>
2021-05-23 14:29:40 +0530

    Update Sdata.jsx

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,6 +2,7 @@
 
 const Sdata = [
     {
+    id: 1,
     imgscr:"https://i.pinimg.com/originals/64/6b/24/646b24208a3ba311a17abb675a1c0ca5.jpg",
     title:"A Netflix Original Series",
     sname:"Dark",
@@ -9,6 +10,7 @@ const Sdata = [
 
     },
     {
+    id: 2,
     imgscr:"https://images-eu.ssl-images-amazon.com/images/I/51BvLPy81sL.__AC_SX300_SY300_QL70_ML2_.jpg",
     title:" A Netflix Original Series",
     sname:"Money Hiest",
@@ -16,6 +18,7 @@ const Sdata = [
 
     },
     {
+    id: 3,
     imgscr:"https://www.trendsinternational.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/7/17293_stranger_things_3_-_group.jpg",
     title:" A Netflix Original Series",
     sname:"Stranger Things",
@@ -23,6 +26,7 @@ const Sdata = [
 
     },
     {
+    id: 4,
     imgscr:"https://images.bhaskarassets.com/web2images/521/2020/03/20/collage_1584698467.jpg",
     title:" A Netflix Original Series",
     sname:"Delhi Crime",
@@ -30,6 +34,7 @@ const Sdata = [
 
     },
     {
+    id: 5,
     imgscr:"https://upload.wikimedia.org/wikipedia/en/7/7a/Sacred_Games_Title.png",
     title:"A Netflix Original Series",
     sname:"Sacred Games",
